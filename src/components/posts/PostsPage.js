865b0809d854ebf9e63a013d2ae2f68fcb020474
Alexander Lind <alex.anonym@gmail.com>
2022-10-11 13:25:23 +0200

    Improved /posts view. Renderes from API correctly.

--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PostForm from './PostForm';
-import client from '../../utils/client';
-import './style.css';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PostForm from "./PostForm";
+import client from "../../utils/client";
+import "./style.css";
 
-import Header from '../Header/Header';
+import Header from "../Header/Header";
 
 const PostsPage = () => {
-  const [post, setPost] = useState({ content: '' });
-  const [postResponse, setPostResponse] = useState('');
+  const [post, setPost] = useState({ content: "" });
+  const [postResponse, setPostResponse] = useState("");
   const [posts, setPosts] = useState([]);
   let navigate = useNavigate();
 
   useEffect(() => {
-    client.get('/posts').then((res) => setPosts(res.data.data.posts));
+    client.get("/posts").then((res) => setPosts(res.data.data.posts));
   }, []);
 
   const createPost = async (event) => {
     event.preventDefault();
     client
-      .post('/post', post)
+      .post("/post", post)
       .then((res) => setPostResponse(res.data))
       .catch((data) => {
         console.log(data);
@@ -37,27 +37,43 @@ const PostsPage = () => {
 
   const signOut = (event) => {
     event.preventDefault();
-    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, '');
-    navigate('../', { replace: true });
+    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, "");
+    navigate("../", { replace: true });
   };
+
+  console.log(posts, postResponse);
 
   return (
     <>
       <Header companyName={`Cohort Manager 2.0`} />
-      <section className='posts-section'>
-        <button id='user-signout-button' onClick={signOut}>
-          sign out
-        </button>
-        <p>Status: {postResponse.status}</p>
-        <PostForm handleSubmit={createPost} handleChange={handleChange} />
-        <ul className='posts-list'>
-          {posts.map((post, index) => (
-            <li key={index} className='post-item'>
-              {post.content}
-            </li>
-          ))}
-        </ul>
-      </section>
+      <main>
+        <section className="posts-section">
+          <button id="user-signout-button" onClick={signOut}>
+            sign out
+          </button>
+          <p>Status: {postResponse.status}</p>
+          
+          <ul className="posts-list">
+            {posts.map((post, index) => (
+              <li key={index} className="post-item">
+                <div className="post-item-user">
+                {`${post.user.profile.firstName} ${post.user.profile.lastName} says:`} 
+                </div>
+                <div className="post-item-content">
+                {post.content} 
+                </div>
+                <div className="post-item-buttons">
+                <button>Like</button>
+                <button>Comment</button>
+                <button>Delete</button>
+                <button>Edit</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <PostForm handleSubmit={createPost} handleChange={handleChange} />
+        </section>
+      </main>
     </>
   );
 };
