24560a5c8bd9d434f1c58c733f61657c212a756f
Paolo Federle <federlepaolo95@gmail.com>
2022-10-19 17:56:01 +0200

    added like functionality for the post and basic style for liked button

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -2,11 +2,96 @@ import { useState } from 'react'
 import PostComments from './PostComments'
 import PostCommentsForm from './PostCommentsForm'
 import client from '../../utils/client'
+import { useEffect } from 'react'
 
 const Post = (props) => {
+  const { post, setPostResponse, index, userData } = props
   const [addComment, setAddComment] = useState(false)
   const [comment, setComment] = useState({})
-  const { post, setPostResponse, index, userData } = props
+  const [likes, setLikes] = useState(post.postLikes)
+  const [likesCount, setLikesCount] = useState(0)
+  const [likedItem, setLikedItem] = useState(null)
+
+  // console.log('post is', post)
+  // console.log('post.postLikes is', post.postLikes)
+  // console.log('likes is', likes)
+  // console.log('post.postLikes.length is', post.postLikes.length)
+
+  useEffect(() => {
+    if (likes.length > 0) {
+      likes.forEach((item) => {
+        if (item.userId === userData.id) {
+          setLikedItem(item)
+        }
+        // console.log('useEffect was called', likesCount)
+      })
+    }
+    likeCounter()
+  }, [])
+
+  function likePost() {
+    const url = `/post/${post.id}/postLike`
+    const data = {
+      active: true,
+      postLikeId: likedItem.id
+    }
+    client.post(url, data).then((res) => {
+      setLikes(res.data.data)
+      setLikesCount(likesCount + 1)
+      const myLikeItem = { ...likedItem, active: true }
+      setLikedItem(myLikeItem)
+    })
+  }
+
+  function firstLikePost() {
+    const url = `/post/${post.id}/postLike`
+    const data = {
+      active: true
+    }
+    client.post(url, data).then((res) => {
+      setLikes(res.data.data)
+      setLikesCount(likesCount + 1)
+      console.log('res.data.data is:', res.data.data)
+      const myLikeItem = res.data.data
+      setLikedItem(myLikeItem)
+    })
+  }
+
+  function removeLike() {
+    const url = `/post/${post.id}/postLike`
+    console.log('likedItem.id is:', likedItem.id)
+    const data = {
+      active: false,
+      postLikeId: likedItem.id
+    }
+    client.post(url, data).then((res) => {
+      setLikes(res.data.data)
+      setLikesCount(likesCount - 1)
+      const myLikeItem = { ...likedItem, active: false }
+      setLikedItem(myLikeItem)
+    })
+  }
+
+  const handleClick = () => {
+    if (likedItem) {
+      if (likedItem.active) {
+        // console.log('like is active and will get removed from: ', item)
+        removeLike()
+        // console.log('removed like: ', post.postLikes)
+        return
+        // console.log('active is true')
+      } else {
+        // console.log('like is not active and will get activated from: ', item)
+        likePost()
+        // console.log('added like: ', post.postLikes)
+        return
+      }
+    } else {
+      // console.log('found nothing, add a like')
+      firstLikePost()
+      // console.log(post.postLikes)
+    }
+  }
 
   const handleChange = (event) => {
     event.preventDefault()
@@ -36,6 +121,26 @@ const Post = (props) => {
     event.target.reset()
   }
 
+  function likeCounter() {
+    let newCounter = 0
+    if (likes.length) {
+      likes.forEach((item) => {
+        if (item.active) {
+          newCounter++
+        }
+      })
+    }
+    setLikesCount(newCounter)
+  }
+
+  function likeStyleCheck() {
+    console.log('function triggered 1')
+    if (likedItem && likedItem.active) {
+      console.log('function triggered 2')
+      return 'like-blue'
+    }
+  }
+
   return (
     <li className="post-item">
       <div className="post-item-user">
@@ -43,7 +148,10 @@ const Post = (props) => {
       </div>
       <div className="post-item-content">{post.content}</div>
       <div className="post-item-buttons" key={index}>
-        <button>Like</button>
+        <button className={`${likeStyleCheck()}`} onClick={handleClick}>
+          <span>{`Like | ${likesCount}`}</span>
+        </button>
+
         <button
           id={post.id}
           onClick={(e) =>
