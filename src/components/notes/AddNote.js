45746c6b254b2db90164e865c0b65024aa7b51ad
mbalabanov <marincomics@gmail.com>
2022-10-21 11:40:08 +0300

    Corrects sidebar userData issue, adds sidebar to notes pages

--- src/components/notes/AddNote.js
+++ src/components/notes/AddNote.js
@@ -4,6 +4,7 @@ import client from '../../utils/client'
 import Button from '@mui/material/Button'
 import Header from '../Header/Header'
 import { useNavigate } from 'react-router-dom'
+import SideNavBar from '../sideNavBar/sideNavBar'
 import './style.css'
 
 function AddNote({ userData }) {
@@ -53,38 +54,41 @@ function AddNote({ userData }) {
   }
 
   return (
-    <>
-      <Header userData={userData} />
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <h2>Add a Note</h2>
-      <p>Please select a student ID and enter the note text</p>
-      <form className="user-form noteInputForm" onSubmit={handleNoteSubmit}>
-        <select name="userId" id="userId" onChange={handleNoteChange}>
-          {students.map((student, index) => (
-            <option
-              key={index}
-              label={`${student.firstName} ${student.lastName} (${student.id})`}
+    <div className="content ">
+      <Header companyName={`Cohort Manager 2.0`} userData={userData} />
+      <div className="mainGridArea ">
+        <SideNavBar />
+        <section className="main-col">
+          <h2>Add a Note</h2>
+          <p>Please select a student ID and enter the note text</p>
+          <form className="user-form noteInputForm" onSubmit={handleNoteSubmit}>
+            <select name="userId" id="userId" onChange={handleNoteChange}>
+              {students.map((student, index) => (
+                <option
+                  key={index}
+                  label={`${student.firstName} ${student.lastName} (${student.id})`}
+                  variant="outlined"
+                  value={student.id}
+                ></option>
+              ))}
+            </select>
+            <br />
+            <TextField
+              className="user-form-input"
+              type="text"
+              label="Content text"
               variant="outlined"
-              value={student.id}
-            ></option>
-          ))}
-        </select>
-        <br />
-        <TextField
-          className="user-form-input"
-          type="text"
-          label="Content text"
-          variant="outlined"
-          name="content"
-          onChange={handleNoteChange}
-        />
-        <br />
-        <Button id="user-submit-button" type="submit" variant="contained">
-          Submit
-        </Button>
-      </form>
-    </>
+              name="content"
+              onChange={handleNoteChange}
+            />
+            <br />
+            <Button id="user-submit-button" type="submit" variant="contained">
+              Submit
+            </Button>
+          </form>
+        </section>
+      </div>
+    </div>
   )
 }
 
--- src/components/notes/NotesPage.js
+++ src/components/notes/NotesPage.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import Header from '../Header/Header'
 import { Link } from 'react-router-dom'
 import StudentNotes from './StudentNotes'
+import SideNavBar from '../sideNavBar/sideNavBar'
 
 function NotesPage({ userData }) {
   const [students, setStudents] = useState([])
@@ -23,31 +24,36 @@ function NotesPage({ userData }) {
   }, [students])
 
   return (
-    <>
-      <Header userData={userData} />
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <h2>Teacher Notes for Students</h2>
-      <Link to="/add-note" style={{ textDecoration: 'none' }}>
-        <Button
-          sx={{
-            height: '20px',
-            marginTop: '2px',
-            fontSize: '13px'
-          }}
-          className="header-button"
-          variant="contained"
-        >
-          Add Note
-        </Button>
-      </Link>
-      {students.map((student, index) => (
-        <p key={index}>
-          {student.firstName} {student.lastName} (User ID: {student.id})
-          <StudentNotes studentId={student.id} />
-        </p>
-      ))}
-    </>
+    <div className="content ">
+      <Header companyName={`Cohort Manager 2.0`} userData={userData} />
+      <div className="mainGridArea ">
+        <SideNavBar />
+        <section className="main-col">
+          <h2>Teacher Notes for Students</h2>
+          <Link to="/add-note" style={{ textDecoration: 'none' }}>
+            <Button
+              sx={{
+                height: '20px',
+                marginTop: '2px',
+                fontSize: '13px'
+              }}
+              className="header-button"
+              variant="contained"
+            >
+              Add Note
+            </Button>
+          </Link>
+          {students.map((student, index) => (
+            <div key={index}>
+              <p>
+                {student.firstName} {student.lastName} (User ID: {student.id})
+              </p>
+              <StudentNotes studentId={student.id} />
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
   )
 }
 
