6718d595b81a5ddcaf23ab37ab37f72b2c01739d
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-25 12:07:26 +0000

    Temporary Add Popup create, basic CSS

--- src/components/Cohorts/cohortsList/CohortsList.js
+++ src/components/Cohorts/cohortsList/CohortsList.js
@@ -1,31 +1,48 @@
 import client from '../../../utils/client'
 import { useState, useEffect } from 'react'
+import CohortListItem from './cohortListItem/CohortListItem'
+import './style.css'
 
 function CohortsList() {
   const [renderCohortPopup, setRenderCohortPopup] = useState(false)
   const [cohortsResponse, setCohortsResponse] = useState([])
 
+  // GET the Cohorts List from db
   useEffect(() => {
     client
       .get('/cohorts')
-      .then((res) => {
-        setCohortsResponse(res.data)
-        console.log(res.data)
-      })
+      .then((res) => setCohortsResponse(res.data))
       .catch((err) => console.log(err.response))
   }, [])
 
   return (
-    <section>
-      <button>Add Cohort</button>
+    <section className="cohorts-list-panel">
+      {/* Conditional Rendering - To Render the Add Popup #115 */}
+      {renderCohortPopup ? (
+        <div className="DEBUG-popup">
+          <h2>Temporary Add popup</h2>
+          <button
+            onClick={() => {
+              setRenderCohortPopup(false)
+            }}
+          >
+            Close popup
+          </button>
+        </div>
+      ) : null}
+
+      <button
+        onClick={() => {
+          // When the state is true, the popup will appear
+          setRenderCohortPopup(true)
+        }}
+      >
+        Add Cohort
+      </button>
       {/* Render CohortListItem mapping though the Cohorts List */}
       {cohortsResponse.length !== 0 ? (
         cohortsResponse.data.map((cohort, index) => {
-          return (
-            <div key={index}>
-              <p>CohortName: {cohort.cohortName}</p>
-            </div>
-          )
+          return <CohortListItem cohort={cohort} key={index} />
         })
       ) : (
         <p>Loading Cohorts...</p>
--- src/components/Cohorts/cohortsList/cohortListItem/CohortListItem.js
+++ src/components/Cohorts/cohortsList/cohortListItem/CohortListItem.js
@@ -1,5 +1,11 @@
-function CohortListItem() {
-  return
+import './style.css'
+
+function CohortListItem({ cohort }) {
+  return (
+    <div className="cohort-item-panel">
+      <p>CohortName: {cohort.cohortName}</p>
+    </div>
+  )
 }
 
 export default CohortListItem
