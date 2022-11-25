9c8a64fbf9632adc20a9f16bfdaef0227acf121a
mlync87 <martylynch35@hotmail.com>
2022-11-25 17:06:43 +0000

    made suggested changes

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -6,135 +6,133 @@ import Header from '../Header/Header'
 import './styles/ProfilePage.css'
 
 function ProfilePage({ loggedInUser }) {
-  const [profilePageUser, setprofilePageUser] = useState(null)
+  const [profilePageUser, setProfilePageUser] = useState(null)
   // TODO: Add loading state
 
   const { id } = useParams()
 
   useEffect(() => {
     client.get(`/users/${id}`).then((data) => {
-      setprofilePageUser(data.data.data.user)
+      setProfilePageUser(data.data.data.user)
     })
     // TODO: Change loading state here
   }, [id])
 
   if (profilePageUser === null) {
-    return <p> Loading...</p>
+    return <> </>
   }
 
-  if (profilePageUser !== null) {
-    return (
-      <>
-        <Header loggedInUser={loggedInUser} />
-        <h2>Profile</h2>
-        <div className="container">
-          <div className="profile-header">
-            <img src={profilePageUser.profileUrl} alt="Profile img" />
-            <div>
-              <h2>
-                {profilePageUser.firstName} {profilePageUser.lastName}
-              </h2>
-              <p>{profilePageUser.role}</p>
-            </div>
-          </div>
-          <div className="edit">
-            <Link to={`/profile/${loggedInUser.id}/edit`}>
-              <button
-                className={
-                  loggedInUser.role !== 'TEACHER' ||
-                  (loggedInUser.role === 'STUDENT' &&
-                    profilePageUser.id !== loggedInUser.id)
-                    ? 'button buttonDisabled'
-                    : 'button'
-                }
-              >
-                edit
-              </button>
-            </Link>
-          </div>
-          <div className="basic-info">
-            <hr />
-            <h2>Basic Info</h2>
-            <ul>
-              <li>First Name: {profilePageUser.firstName}</li>
-              <li>Last Name: {profilePageUser.lastName}</li>
-              <li>
-                <span className="space">Username:</span>
-                {profilePageUser.username
-                  ? profilePageUser.username
-                  : 'No username to display'}
-              </li>
-              <li>
-                <span className="space">Github:</span>
-                {profilePageUser.githubUrl
-                  ? profilePageUser.githubUrl
-                  : 'No link to display'}
-              </li>
-            </ul>
-          </div>
-          <div className="training-info">
-            <hr />
-            <h2>Training Info</h2>
-            <ul>
-              <li>
-                <span className="space">Role:</span>
-                {profilePageUser.role
-                  ? profilePageUser.role
-                  : 'No role to display'}
-              </li>
-              <li>
-                <span className="space">Specialism:</span>
-                {profilePageUser.specialism
-                  ? profilePageUser.specialism
-                  : 'No specialism to display'}
-              </li>
-              <li>
-                <span className="space">Start Date:</span>
-                {profilePageUser.startDate
-                  ? profilePageUser.startDate
-                  : 'No date to display'}
-              </li>
-              <li>
-                <span className="space">End Date:</span>
-                {profilePageUser.endDate
-                  ? profilePageUser.endDate
-                  : 'No date to display'}
-              </li>
-            </ul>
-          </div>
-          <div className="contact-info">
-            <hr />
-            <h2>Contact Info</h2>
-            <ul>
-              <li>
-                <span className="space">Email:</span>
-                {profilePageUser.email
-                  ? profilePageUser.email
-                  : 'No email to display'}
-              </li>
-              <li>
-                <span className="space">Mobile:</span>
-                {profilePageUser.mobile
-                  ? profilePageUser.mobile
-                  : 'No number to display'}
-              </li>
-              <li>
-                <span className="space">Password:</span>
-                {profilePageUser.password
-                  ? profilePageUser.password
-                  : 'No password to display'}
-              </li>
-            </ul>
-          </div>
-          <div className="bio">
-            <hr />
-            <h2>Bio</h2>
-            <p>{profilePageUser.biography}</p>
+  return (
+    <>
+      <Header loggedInUser={loggedInUser} />
+      <h2>Profile</h2>
+      <div className="container">
+        <div className="profile-header">
+          <img src={profilePageUser.profileUrl} alt="Profile img" />
+          <div>
+            <h2>
+              {profilePageUser.firstName} {profilePageUser.lastName}
+            </h2>
+            <p>{profilePageUser.role}</p>
           </div>
         </div>
-      </>
-    )
-  }
+        <div className="edit">
+          <Link to={`/profile/${loggedInUser.id}/edit`}>
+            <button
+              className={
+                loggedInUser.role !== 'TEACHER' ||
+                (loggedInUser.role === 'STUDENT' &&
+                  profilePageUser.id !== loggedInUser.id)
+                  ? 'button buttonDisabled'
+                  : 'button'
+              }
+            >
+              edit
+            </button>
+          </Link>
+        </div>
+        <div className="basic-info">
+          <hr />
+          <h2>Basic Info</h2>
+          <ul>
+            <li>First Name: {profilePageUser.firstName}</li>
+            <li>Last Name: {profilePageUser.lastName}</li>
+            <li>
+              <span className="space">Username:</span>
+              {profilePageUser.username
+                ? profilePageUser.username
+                : 'No username to display'}
+            </li>
+            <li>
+              <span className="space">Github:</span>
+              {profilePageUser.githubUrl
+                ? profilePageUser.githubUrl
+                : 'No link to display'}
+            </li>
+          </ul>
+        </div>
+        <div className="training-info">
+          <hr />
+          <h2>Training Info</h2>
+          <ul>
+            <li>
+              <span className="space">Role:</span>
+              {profilePageUser.role
+                ? profilePageUser.role
+                : 'No role to display'}
+            </li>
+            <li>
+              <span className="space">Specialism:</span>
+              {profilePageUser.specialism
+                ? profilePageUser.specialism
+                : 'No specialism to display'}
+            </li>
+            <li>
+              <span className="space">Start Date:</span>
+              {profilePageUser.startDate
+                ? profilePageUser.startDate
+                : 'No date to display'}
+            </li>
+            <li>
+              <span className="space">End Date:</span>
+              {profilePageUser.endDate
+                ? profilePageUser.endDate
+                : 'No date to display'}
+            </li>
+          </ul>
+        </div>
+        <div className="contact-info">
+          <hr />
+          <h2>Contact Info</h2>
+          <ul>
+            <li>
+              <span className="space">Email:</span>
+              {profilePageUser.email
+                ? profilePageUser.email
+                : 'No email to display'}
+            </li>
+            <li>
+              <span className="space">Mobile:</span>
+              {profilePageUser.mobile
+                ? profilePageUser.mobile
+                : 'No number to display'}
+            </li>
+            <li>
+              <span className="space">Password:</span>
+              {profilePageUser.password
+                ? profilePageUser.password
+                : 'No password to display'}
+            </li>
+          </ul>
+        </div>
+        <div className="bio">
+          <hr />
+          <h2>Bio</h2>
+          <p>{profilePageUser.biography}</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default ProfilePage
