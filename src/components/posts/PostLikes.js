eb2c03946a8df264dd2b5aea27e77d546a3b5488
Fatima <fxtimaahmed14@gmail.com>
2022-11-30 12:12:57 +0000

    basic plan for like posts  written

---
+++ src/components/posts/PostLikes.js
@@ -0,0 +1,13 @@
+// This is where the component for liking will live
+// A function called PostLikes => no props at this moment
+// Need to take the whole jsx section for likes
+// The return () for this function will contain jsx of like button + interpolate values of likes
+// NameClass for a toggle function that works with css to make like button blue will also need to be pressnt in the jsx
+// {need to go over toggling and conditional rendering}
+
+// need to export the function to PostPages.js + call it in the right place
+// This will make me cry
+
+export default function PostLikes() {
+  return <>{/* where the jsx will sit */}</>
+}
