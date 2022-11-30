fb6b50cb0357eb4f63f39771ff39a03ee74e6baf
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-11-30 11:02:55 +0000

    removed nav placeholder

--- src/components/Cohorts/CohortsPage/CohortsPageTeacher.js
+++ src/components/Cohorts/CohortsPage/CohortsPageTeacher.js
@@ -10,32 +10,6 @@ function CohortsPageTeachers({ loggedInUser }) {
 
       {/*may not use nav bar*/}
       <main className="teachers-cohorts-main">
-        <nav className="teachers-cohorts-nav">
-          <ul className="teachers-cohorts-ul">
-            <li className="teacher-cohorts-li">
-              <span className="material-symbols-outlined">Home</span>Home
-            </li>
-            <li className="teacher-cohorts-li">
-              <span className="material-symbols-outlined">Account_Circle</span>
-              Profile
-            </li>
-            <li className="teacher-cohorts-li">
-              <span className="material-symbols-outlined">School</span>Cohorts
-            </li>
-            <li className="teacher-cohorts-li">
-              <span className="material-symbols-outlined">Task</span>Exercises
-            </li>
-
-            <hr className="teachers-cohorts-hr"></hr>
-            <li className="teacher-cohorts-li">
-              <span className="material-symbols-outlined">Notes</span>Notes
-            </li>
-            <li className="teacher-cohorts-li">
-              <span className="material-symbols-outlined">Analytics</span>Logs
-            </li>
-          </ul>
-        </nav>
-
         <section className="content">
           <h2 className="teachers-cohorts-h2">
             <span className="teachers-cohorts-title1">Cohorts</span>
