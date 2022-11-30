301d1c71f88782e9bb78b5632eaffa9cdf608c2e
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-30 17:24:06 +0000

    Some changes

--- src/components/NavigationRail/NavigationRail.js
+++ src/components/NavigationRail/NavigationRail.js
@@ -1,24 +1,44 @@
+import { useNavigate } from 'react-router-dom'
 import './NavigationRail.css'
 
 function NavigationRail({ user }) {
+  const navigate = useNavigate()
+
+  function homePressed() {
+    navigate(`/posts`)
+  }
+  function profilePressed() {
+    navigate(`/profile/${user.id}`)
+  }
+  function cohortPressed() {
+    if (user.role === 'TEACHER') {
+      navigate('/cohorts')
+    } else {
+      navigate(`/cohorts/${user.cohortId}`)
+    }
+  }
+
   return (
     <nav className="navigation-rail">
       <ul className="nav-item-wrapper">
-        <li className="nav-item">
+        {/* Home */}
+        <li className="nav-item noselect" onClick={homePressed}>
           <div className="nav-item-circle">
             <span className="material-symbols-outlined">home</span>
           </div>
           <span className="nav-item-name">Home</span>
         </li>
 
-        <li className="nav-item">
+        {/* Profile */}
+        <li className="nav-item noselect" onClick={profilePressed}>
           <div className="nav-item-circle">
             <span className="material-symbols-outlined">person</span>
           </div>
           <span className="nav-item-name">Profile</span>
         </li>
 
-        <li className="nav-item">
+        {/* Cohort */}
+        <li className="nav-item noselect" onClick={cohortPressed}>
           <div className="nav-item-circle">
             <span className="material-symbols-outlined">workspaces</span>
           </div>
