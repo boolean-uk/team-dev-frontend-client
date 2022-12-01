e1798b68c3f81b76e187ed178af444e053319822
George Roth <g@georgeroth.eu>
2022-12-01 16:19:36 +0000

    Adding dynamic like and edit links

--- src/components/posts/CommentEditButton.js
+++ src/components/posts/CommentEditButton.js
@@ -1,15 +1,29 @@
+import { useState } from 'react'
+
 export default function CommentEditButton({ comment, loggedInUser }) {
+  const [showEditandDelete, setShowEditandDelete] = useState(false)
+
   const editButtonClicked = () => {
     console.log(`Edit button has been clicked at comment number ${comment.id}`)
+    setShowEditandDelete(!showEditandDelete)
+    console.log(showEditandDelete)
   }
   return (
-    <div
-      className="comment-edit-button"
-      onClick={() => {
-        editButtonClicked()
-      }}
-    >
-      ...
-    </div>
+    <>
+      <div
+        className="comment-edit-button"
+        onClick={() => {
+          editButtonClicked()
+        }}
+      >
+        ...
+      </div>
+      {showEditandDelete && (
+        <>
+          <div className="editButton">edit</div>
+          <div className="deleteButton">delete</div>
+        </>
+      )}
+    </>
   )
 }
