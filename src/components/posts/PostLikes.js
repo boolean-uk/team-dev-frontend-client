948a4f67f3830df24186ae9e7420979feea16195
Fatima <fxtimaahmed14@gmail.com>
2022-12-02 12:37:24 +0000

    like count udpates with like

--- src/components/posts/PostLikes.js
+++ src/components/posts/PostLikes.js
@@ -5,8 +5,8 @@ import client from '../../utils/client'
 export default function PostLikes() {
   const [likeCount, setLikeCount] = useState(0)
 
-  // currently only checking likes on postId  6
-  // 6 needs to changed to current postId using string interpolation
+  // currently only checking likes on postId  6 => needs to changed to current postId using string interpolation
+
   useEffect(() => {
     client.get('/posts/postLike').then((data) => {
       const allLikes = data.data.data.postLikes
@@ -17,34 +17,31 @@ export default function PostLikes() {
       })
       setLikeCount(filterLikes.length)
     })
-  }, [])
-  //  ^ not sure what the use Effect should be refreshed on yet
-  //   this useEffect is used to display like count
+  }, [setLikeCount])
 
-  // need to interpolate acc post id into this later
-  const LikeandUnlikingPost = () => {
-    client.post('/posts/6/postLike').then((data) => {
-      const postLikeData = data.data.data
-      if (postLikeData.active === false) {
-        return true
-      }
-    })
+  // need to interpolate actual post id into this later
+  const handleLikingandUnlikingPost = () => {
+    client
+      .post('/posts/6/postLike')
+      .then((data) => {
+        const postLikeData = data.data.data
+        if (postLikeData.active === false) {
+          return true
+        }
+      })
+      .then((updatedPostLikeData) => setLikeCount(updatedPostLikeData))
   }
 
-  //  both liking and unliking in one big fucntion with 1 if statement that checks wether like is active or not => POST
-
-  //  LIKING A POST:
-  //   need to map through the post object and check for its active status
-  //   create function to use in  onClick that sends POST request when heart icon is clicked
-  //   request must be sent to posts/{postId}/postLikes { for now postId = 6}
+  //  both liking and unliking in one big fucntion with 1 if statement that checks whether like is active or not => POST
 
   //   UN-LIKING A POST:
+  //   request must be sent to posts/{postId}/postLikes { for now postId = 6}
   //   else
-  //    if statements => not active
+  //   not active
 
   return (
     <div className="like-container">
-      <div className="like-icon" onClick={LikeandUnlikingPost}></div>
+      <div className="like-icon" onClick={handleLikingandUnlikingPost}></div>
       <div className="like">Like</div>
       <div className="number-of-likes">{likeCount} likes</div>
     </div>
@@ -59,6 +56,6 @@ export default function PostLikes() {
 // i am guessing once I have made it true then it should just like the post???
 // need to check live server to see if it works
 
-// onle needs one else to capture all unlikes
+// only needs one else to capture all unlikes
 // unlike will also be a POST not sure about the code in the slightest
 // will need some check???
