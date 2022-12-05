3cdd6a6f3675b7cba67f9cd68cf5bfd893b7577e
Joel Watkins <eck087@yahoo.co.uk>
2022-12-05 13:01:51 +0000

    added cohort select dropdown to search results

--- src/components/search/SearchResultPerson.js
+++ src/components/search/SearchResultPerson.js
@@ -1,7 +1,19 @@
 import './styles/SearchResult.css'
 import { FaEllipsisH } from 'react-icons/fa'
+import client from '../../utils/client'
+import { useNavigate } from 'react-router-dom'
 
-function SearchResultPerson({ loggedInUser, person }) {
+function SearchResultPerson({ loggedInUser, person, cohorts }) {
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    const cohort = parseInt(e.target.value)
+
+    client
+      .patch(`/users/update/${person.id}`, { ...person, cohortId: cohort })
+      .then((data) => navigate(`/profile/${person.id}`))
+  }
+
   return (
     <div className="search--result_item">
       <img src={person.profileUrl} alt="Profile img" />
@@ -13,7 +25,25 @@ function SearchResultPerson({ loggedInUser, person }) {
         <button className="button">Profile</button>
         {loggedInUser.role === 'TEACHER' && (
           <>
-            <button className="button">Add to cohort</button>
+            {cohorts && (
+              <>
+                <select
+                  className="search--result_dropdown"
+                  name="cohortId"
+                  onChange={handleChange}
+                >
+                  <option>Change Cohort...</option>
+                  {cohorts.map((cohort) => {
+                    const { id, cohortName } = cohort
+                    return (
+                      <option key={id} value={id}>
+                        {cohortName}
+                      </option>
+                    )
+                  })}
+                </select>
+              </>
+            )}
             <button className="button">Add note</button>
           </>
         )}
